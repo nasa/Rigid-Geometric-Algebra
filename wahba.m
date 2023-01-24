b353928da0e107a197447f25b46d6b26e6f502a1
Carpenter, Russell (GSFC-4440) <russell.carpenter@nasa.gov>
2023-01-24 11:33:02 -0500

    Improvements to wahba test function

--- wahba.m
+++ wahba.m
@@ -8,6 +8,7 @@ function Q = wahba(M,N)
 
 if nargin < 1
     test
+    Q = @test;
     return
 end
 
@@ -140,8 +141,23 @@ function a2 = reject(a,b)
 a2 = a - dot(a,b)/dot(b,b)*b;
 end
 
-function test
+function test(opts)
 %% Test Wahba
+arguments
+    opts.points (1,1) double = 1
+    opts.point_sigma (1,1) double = 0
+    opts.planes (1,1) double = 1
+    opts.plane_sigma (1,1) = 0
+    opts.directions (1,1) double = 1
+    opts.direction_sigma (1,1) double = 0
+    opts.lines (1,1) = 1
+    opts.line_sigma (1,1) double = 0
+    opts.motors (1,1) double = 1
+    opts.motor_sigma (1,1) double = 0
+    opts.rotation_sigma (1,1) double = 0
+    opts.translation_sigma (1,1) double = 0
+    opts.axis_sigma (1,1) double = 0;
+end
 %Ltru = unitize(rgaline);
 %phi = pi*rand - pi;
 %d = 10*randn;
@@ -154,36 +170,71 @@ function test
 %Q0 = rgamotor(R0*S0);
 %Qtru = rgamotor(R*S);
 Qtru = unitize(rgamotor); Qtru.anti = true;
-[~,~,Ltru] = extract(Qtru);
-n = 4;
+[~,~,vtru,mtru] = extract(Qtru);
+n = opts.points + opts.planes + opts.directions + opts.lines + opts.motors;
 for i = n:-1:1
-    phi = 0*pi/4*randn;
-    d = 0*3*randn;
-    dR = unitize(rgamotor(phi,0,Ltru)); % rotation error
-    dS = rgamotor(0,d,Ltru); % translation error
+    pherr = opts.rotation_sigma*randn;
+    derr = opts.translation_sigma*randn;
+    vper = vtru + opts.axis_sigma*randn(1,3);
+    mper = mtru + opts.axis_sigma*randn(1,3);
+    mper = reject(mper,vper);
+    Lper = unitize(rgaline(vper,mper)); % perturbed axis (dir & mom)
+    dR = unitize(rgamotor(pherr,0,Lper)); % rotation error
+    dS = rgamotor(0,derr,Lper); % translation error
     dR.anti = true; dS.anti = true;
     dQ = rgamotor(dR*dS); % motor error
     Q = dQ*Qtru*~dQ;
-    a(i) = unitize(rgapoint); a(i).anti = true;
-    b(i) = rgapoint(Q*a(i)*~Q); b(i).anti = true;
-    f(i) = unitize(rgaplane); % anti by default
-    g(i) = rgaplane(Q*f(i)*~Q);
-    u(i) = rgaline(unit(randn(1,3)),zeros(1,3)); u(i).anti = true;
-    v(i) = rgaline(Q*u(i)*~Q); v(i).anti = true;
-    K(i) = unitize(rgaline); K(i).anti = true;
-    L(i) = rgaline(Q*K(i)*~Q); L(i).anti = true;
-    R(i) = unitize(rgamotor); R(i).anti = true;
-    S(i) = rgamotor(Q*R(i)*~Q); S(i).anti = true;
-    M{i,1} = a(i); N{i,1} = b(i); % points
-    M{i,2} = f(i); N{i,2} = g(i); % planes
-    M{i,3} = u(i); N{i,3} = v(i); % directions
-    M{i,4} = K(i); N{i,4} = L(i); % lines
-    M{i,5} = R(i); N{i,5} = S(i); % motors
+    if opts.points
+        M{i} = unitize(rgapoint); M{i}.anti = true;
+        N{i} = rgapoint(Q*M{i}*~Q); N{i}.anti = true;
+        if opts.point_sigma
+            N{i}.m = N{i}.m + opts.point_sigma*randn(1,16).*N{i}.m;
+            N{i} = unitize(N{i});
+        end
+        opts.points = opts.points - 1;
+    elseif opts.planes
+        M{i} = unitize(rgaplane); % anti by default
+        N{i} = rgaplane(Q*M{i}*~Q);
+        if opts.plane_sigma
+            N{i}.m = N{i}.m + opts.plane_sigma*randn(1,16).*N{i}.m;
+            N{i} = unitize(N{i});
+        end
+        opts.planes = opts.planes - 1;
+    elseif opts.directions
+        M{i} = rgaline(unit(randn(1,3)),zeros(1,3)); M{i}.anti = true;
+        N{i} = rgaline(Q*M{i}*~Q); N{i}.anti = true;
+        if opts.direction_sigma
+            N{i}.m = N{i}.m + opts.direction_sigma*randn(1,16).*N{i}.m;
+            N{i} = unitize(N{i});
+        end
+        opts.directions = opts.directions - 1;
+    elseif opts.lines
+        M{i} = unitize(rgaline); M{i}.anti = true;
+        N{i} = rgaline(Q*M{i}*~Q); N{i}.anti = true;
+        if opts.line_sigma
+            N{i}.m = N{i}.m + opts.line_sigma*randn(1,16).*N{i}.m;
+            N{i} = unitize(N{i});
+        end
+        opts.lines = opts.lines - 1;
+    elseif opts.motors
+        M{i} = unitize(rgamotor); M{i}.anti = true;
+        N{i} = rgamotor(Q*M{i}*~Q); N{i}.anti = true;
+        if opts.motor_sigma
+            N{i}.m = N{i}.m + opts.motor_sigma*randn(1,16).*N{i}.m;
+            N{i} = unitize(N{i});
+        end
+        opts.motors = opts.motors - 1;
+    end
+    %M{i,1} = a(i); N{i,1} = b(i); % points
+    %M{i,2} = f(i); N{i,2} = g(i); % planes
+    %M{i,3} = u(i); N{i,3} = v(i); % directions
+    %M{i,4} = K(i); N{i,4} = L(i); % lines
+    %M{i,5} = R(i); N{i,5} = S(i); % motors
 end
 %M = M(:);
 %N = N(:);
-M = reshape(M(:,[1:4]),[],1);
-N = reshape(N(:,[1:4]),[],1);
+%M = reshape(M(:,[2 4]),[],1);
+%N = reshape(N(:,[2 4]),[],1);
 
 Qest = wahba(M,N);
 Qest.anti = true;
